bb6bfd1966313ad47d8d6e399d486897d59e32a5
Bhupendra <bhupendra1430@bhu.ac.in>
2025-08-28 15:03:51 +0530

    splash screen setup

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,11 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import LoginScreen from '../src/screens/Login-Signup/LoginScreen'
+import NavigationContainer, { createStaticNavigation } from '@react-navigation/native'
+
+const Stack = createStaticNavigation()
 
 const Index = () => {
     return (
-        <View style={{ flex: 1 }}>
-            <Text>Index</Text>
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={LoginScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
     )
 }
 
